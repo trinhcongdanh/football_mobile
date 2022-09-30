8d3b09a1a2f2dcd0ca1e0491f88e729737c0775e
Huỳnh Bình <huynhbinh@Huynhs-MacBook-Pro.local>
2022-09-30 16:26:37 +0700

    chore: create new branch base on master

--- src/app/screens/splash-screen/SplashScreen.tsx
+++ src/app/screens/splash-screen/SplashScreen.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 import { AppImages } from '@football/app/assets/images';
 import { EnStrings } from '@football/app/i18n/EnStrings';
 import styles from './SplashScreen.styles';
-import { ISplashScreenProps } from './SplashScreen.type';
+// import { ISplashScreenProps } from './SplashScreen.type';
 
-export const SplashScreen = (props: ISplashScreenProps) => {
+export const SplashScreen = (props: any) => {
     return (
         <View style={styles.container}>
             <Image source={AppImages.img_logo} style={styles.image} />
