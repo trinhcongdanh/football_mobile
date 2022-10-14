d67065f4b34463e833e9017cf490856f7d4fb254
huynhbinh6 <binhhx.dev@gmail.com>
2022-10-14 10:28:39 +0700

    chore: set up image picker android

--- src/app/screens/football-settings/SettingsScreen.viewModel.ts
+++ src/app/screens/football-settings/SettingsScreen.viewModel.ts
@@ -1,4 +1,5 @@
 import { useAppNavigator } from '@football/app/routes/AppNavigator.handler';
+import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { launchImageLibrary } from 'react-native-image-picker';
 import { ISettingsScreenProps } from './SettingsScreen.type';
@@ -6,6 +7,7 @@ import { ISettingsScreenProps } from './SettingsScreen.type';
 export const useViewModel = (props: ISettingsScreenProps) => {
     const { t } = useTranslation();
     const { goBack, navigate } = useAppNavigator();
+    const [image, setImage] = useState<any>();
 
     const date = new Date();
 
@@ -17,10 +19,14 @@ export const useViewModel = (props: ISettingsScreenProps) => {
 
     const onImagePicker = async () => {
         const result = await launchImageLibrary({ mediaType: 'photo' });
+        result.assets?.map(item => {
+            setImage(item.uri);
+        });
     };
 
     return {
         date,
+        image,
         t,
         goBack,
         navigate,
