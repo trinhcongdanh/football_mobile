17145eddb59a4d2ddab446edeea727c93110ab87
Huỳnh Bình <huynhbinh@Huynhs-MacBook-Pro.local>
2022-11-02 10:29:37 +0700

    package: add mongodb package

--- src/app/screens/football-welcome/WelcomeScreen.tsx
+++ src/app/screens/football-welcome/WelcomeScreen.tsx
@@ -13,19 +13,39 @@ import { useTranslation } from 'react-i18next';
 import { ScreenName } from '@football/app/utils/constants/enum';
 import { appStyles } from '@football/app/utils/constants/appStyles';
 import { Button } from '@football/app/components/button';
+import { MongoClient, ObjectId } from 'mongodb';
+import { useCallback } from 'react';
+import { useMemo } from 'react';
 import styles from './WelcomeScreen.styles';
 import { IWelcomeScreenProps } from './WelcomeScreen.type';
 
 export const WelcomeScreen = ({ navigation, route }: IWelcomeScreenProps) => {
     const { t, i18n } = useTranslation();
 
+    const uri =
+        'mongodb+srv://dogma_dev:tJQTMB78bPAe3YpJ@football.fa1e6d9.mongodb.net/test?retryWrites=true&w=majority';
+
+    const client = useMemo(() => {
+        return new MongoClient(uri);
+    }, []);
+
+    const getApi = useCallback(async () => {
+        try {
+            console.log(client);
+        } finally {
+            // Ensures that the client will close when you finish/error
+            await client.close();
+        }
+    }, [client]);
+
     useEffect(() => {
+        getApi().catch(console.dir);
         if (i18n.language === 'en') {
             I18nManager.forceRTL(false);
         } else {
             I18nManager.forceRTL(true);
         }
-    }, [i18n.language]);
+    }, [getApi, i18n.language]);
 
     const onNavigateFavTeam = () => {
         navigation.navigate(ScreenName.FavTeamPage, {
