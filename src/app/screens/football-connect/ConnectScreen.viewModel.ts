eb3ea6ae12de9aa09338b9ff6090c8256601d24e
VietNe <nqviet.dev@gmail.com>
2023-02-20 16:52:36 +0700

    fix(ios): build without FB SDK

--- src/app/screens/football-connect/ConnectScreen.viewModel.ts
+++ src/app/screens/football-connect/ConnectScreen.viewModel.ts
@@ -1,20 +1,17 @@
-import { Keyboard, Alert } from 'react-native';
-import { useState, useCallback, useEffect, useRef } from 'react';
 import { useAppNavigator } from '@football/app/routes/AppNavigator.handler';
-import { AuthData, OfflineData, ScreenName } from '@football/app/utils/constants/enum';
-import { useTranslation } from 'react-i18next';
-import { IConnectScreenProps } from './ConnectScreen.type';
-import localStorage from '@football/core/helpers/localStorage';
-import { useMount } from '@football/app/utils/hooks/useMount';
-import { isEmpty, isNil } from 'lodash';
-import { LoginManager, AccessToken, Profile, Settings } from 'react-native-fbsdk-next';
+import { AuthData, ScreenName } from '@football/app/utils/constants/enum';
 import { axiosAuth } from '@football/core/api/auth/axiosAuth';
-import { ACTION, AUTH_URL, TOKEN } from '@football/core/api/auth/config';
-import * as uuid from 'uuid';
-import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
+import { ACTION, AUTH_URL } from '@football/core/api/auth/config';
 import { appleAuth } from '@invertase/react-native-apple-authentication';
-import { env } from 'src/config';
+import { GoogleSignin, statusCodes } from '@react-native-google-signin/google-signin';
+import { isEmpty } from 'lodash';
+import { useCallback, useEffect, useRef, useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { Alert, Keyboard } from 'react-native';
+// import { AccessToken, LoginManager, Profile } from 'react-native-fbsdk-next';
 import { useDispatch, useSelector } from 'react-redux';
+import { env } from 'src/config';
+import { IConnectScreenProps } from './ConnectScreen.type';
 
 export const useViewModel = ({ navigation, route }: IConnectScreenProps) => {
     const { t } = useTranslation();
@@ -36,41 +33,41 @@ export const useViewModel = ({ navigation, route }: IConnectScreenProps) => {
 
     // Facebook
     const connectFacebook = useCallback(async () => {
-        LoginManager.logInWithPermissions(['public_profile', 'email']).then((result: any) => {
-            if (result.isCancelled) {
-            } else {
-                Profile.getCurrentProfile().then(currentProfile => {
-                    if (currentProfile) {
-                        console.log(currentProfile);
-                    }
-                });
-                AccessToken.getCurrentAccessToken().then((data: any) => {
-                    console.log(data.accessToken.toString());
-                });
-            }
-        });
-        try {
-            const { data }: any = await axiosAuth.post(
-                `${AUTH_URL}`,
-                serializeParams({
-                    action: ACTION,
-                    token: tokenLogin[0].token,
-                    call: AuthData.REGISTER,
-                    guest_id: profile[0].tc_user,
-                    guest_guid: guestId[0],
-                    'item[facebook_app_id]': env.FACEBOOK_APPID,
-                    'item[facebook_app_secret]': env.FACEBOOK_SECRET_KEY,
-                }),
-                {
-                    headers: {},
-                }
-            );
-            if (!isEmpty(data)) {
-                console.log(data);
-            }
-        } catch (error: any) {
-            Alert.alert(error);
-        }
+        // LoginManager.logInWithPermissions(['public_profile', 'email']).then((result: any) => {
+        //     if (result.isCancelled) {
+        //     } else {
+        //         Profile.getCurrentProfile().then(currentProfile => {
+        //             if (currentProfile) {
+        //                 console.log(currentProfile);
+        //             }
+        //         });
+        //         AccessToken.getCurrentAccessToken().then((data: any) => {
+        //             console.log(data.accessToken.toString());
+        //         });
+        //     }
+        // });
+        // try {
+        //     const { data }: any = await axiosAuth.post(
+        //         `${AUTH_URL}`,
+        //         serializeParams({
+        //             action: ACTION,
+        //             token: tokenLogin[0].token,
+        //             call: AuthData.REGISTER,
+        //             guest_id: profile[0].tc_user,
+        //             guest_guid: guestId[0],
+        //             'item[facebook_app_id]': env.FACEBOOK_APPID,
+        //             'item[facebook_app_secret]': env.FACEBOOK_SECRET_KEY,
+        //         }),
+        //         {
+        //             headers: {},
+        //         }
+        //     );
+        //     if (!isEmpty(data)) {
+        //         console.log(data);
+        //     }
+        // } catch (error: any) {
+        //     Alert.alert(error);
+        // }
     }, []);
 
     // Google Account
@@ -82,25 +79,25 @@ export const useViewModel = ({ navigation, route }: IConnectScreenProps) => {
     }, []);
 
     const connectGoogle = useCallback(async () => {
-        try {
-            await GoogleSignin.hasPlayServices();
-            await GoogleSignin.signIn().then((result: any) => {
-                console.log(result);
-            });
-        } catch (error: any) {
-            if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-                // user cancelled the login flow
-                Alert.alert('User cancelled the login flow !');
-            } else if (error.code === statusCodes.IN_PROGRESS) {
-                Alert.alert('Signin in progress');
-                // operation (f.e. sign in) is in progress already
-            } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-                Alert.alert('Google play services not available or outdated !');
-                // play services not available or outdated
-            } else {
-                console.log(error);
-            }
-        }
+        // try {
+        //     await GoogleSignin.hasPlayServices();
+        //     await GoogleSignin.signIn().then((result: any) => {
+        //         console.log(result);
+        //     });
+        // } catch (error: any) {
+        //     if (error.code === statusCodes.SIGN_IN_CANCELLED) {
+        //         // user cancelled the login flow
+        //         Alert.alert('User cancelled the login flow !');
+        //     } else if (error.code === statusCodes.IN_PROGRESS) {
+        //         Alert.alert('Signin in progress');
+        //         // operation (f.e. sign in) is in progress already
+        //     } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
+        //         Alert.alert('Google play services not available or outdated !');
+        //         // play services not available or outdated
+        //     } else {
+        //         console.log(error);
+        //     }
+        // }
     }, []);
 
     // Apple Developer
