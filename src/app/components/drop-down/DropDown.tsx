0727c2bdfb725d97433e6196b982ca66203cdd07
VietNe <nqviet.dev@gmail.com>
2023-03-06 22:12:50 +0700

    fix(DropDown): clicking outside will close modal

--- src/app/components/drop-down/DropDown.tsx
+++ src/app/components/drop-down/DropDown.tsx
@@ -4,7 +4,7 @@ import { appColors } from '@football/app/utils/constants/appColors';
 import { appStyles } from '@football/app/utils/constants/appStyles';
 import { getSize } from '@football/app/utils/responsive/scale';
 import React from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import IconIonicons from 'react-native-vector-icons/Ionicons';
 import { styles } from './DropDown.style';
 import { IDropDownProps } from './DropDown.type';
@@ -12,6 +12,7 @@ import { IDropDownProps } from './DropDown.type';
 export const DropDown = ({ data, handleSelected, handleCloseModal }: IDropDownProps) => {
     return (
         <View style={styles.background_opacity}>
+            <TouchableOpacity style={StyleSheet.absoluteFillObject} onPress={handleCloseModal} />
             <View style={[appStyles.flex, appStyles.main_container, styles.drop_down]}>
                 <View style={{ marginHorizontal: getSize.m(18) }}>
                     <TouchableOpacity style={styles.close_drop_down} onPress={handleCloseModal} />
