5bf5e16cf3fe91d1886ef4144e7cb605123b7c61
Quy Nguyen <phuquy.uit@gmail.com>
2023-03-17 15:15:18 +0700

    fix bug videos full screen

--- src/app/components/video/Video.tsx
+++ src/app/components/video/Video.tsx
@@ -101,8 +101,8 @@ export const Video = () => {
         } else {
             setFullscreen(true);
             setShowControls(true);
-            Orientation.lockToLandscapeLeft();
         }
+        Orientation.lockToLandscapeLeft();
     };
 
     const handleOrientation = (orientation: string) => {
