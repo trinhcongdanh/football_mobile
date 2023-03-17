eba1a1e3f5b6dd247230ec43c71d103c8bf34a29
Quy Nguyen <phuquy.uit@gmail.com>
2023-03-17 11:33:04 +0700

    fix ios close button in sidebar

--- src/app/routes/side-bar/SideBar.tsx
+++ src/app/routes/side-bar/SideBar.tsx
@@ -3,20 +3,31 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { SideMenu } from '@football/app/components/side-menu/SideMenu';
 import { ScreenName } from '@football/app/utils/constants/enum';
 import { BottomTabStack } from '@football/app/routes/BottomTab';
+import { SafeAreaView } from 'react-native';
+import { appStyles } from '@football/app/utils/constants/appStyles';
 
 const Drawer = createDrawerNavigator();
 
 export const SideBar = () => {
     return (
-        <Drawer.Navigator
-            initialRouteName={ScreenName.BottomTab}
-            screenOptions={{
-                headerShown: false,
-                drawerStyle: { width: '85%' },
-            }}
-            drawerContent={props => <SideMenu {...props} />}
+        <SafeAreaView
+            style={[
+                appStyles.safe_area,
+                {
+                    zIndex: 100000,
+                },
+            ]}
         >
-            <Drawer.Screen name={ScreenName.BottomTab} component={BottomTabStack} />
-        </Drawer.Navigator>
+            <Drawer.Navigator
+                initialRouteName={ScreenName.BottomTab}
+                screenOptions={{
+                    headerShown: false,
+                    drawerStyle: { width: '85%' },
+                }}
+                drawerContent={props => <SideMenu {...props} />}
+            >
+                <Drawer.Screen name={ScreenName.BottomTab} component={BottomTabStack} />
+            </Drawer.Navigator>
+        </SafeAreaView>
     );
 };
