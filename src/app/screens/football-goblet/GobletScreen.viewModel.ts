588a62d4e7c6c9f22d722093f33890568f086e57
trinhcongdanh <congdanhtrinh.dev@gmail.com>
2023-03-24 16:06:22 +0700

    fix: fix bug high

--- src/app/screens/football-goblet/GobletScreen.viewModel.ts
+++ src/app/screens/football-goblet/GobletScreen.viewModel.ts
@@ -3,8 +3,9 @@ import { useAppNavigator } from '@football/app/routes/AppNavigator.handler';
 import { IGobletScreenProps } from '@football/app/screens/football-goblet/GobletScreen.type';
 import { ScreenName } from '@football/app/utils/constants/enum';
 import { CupModel } from '@football/core/models/CupModelResponse';
-import { useCups } from '@football/core/services/Cups.service';
-import { useEffect, useState } from 'react';
+import cupsService, { useCups } from '@football/core/services/Cups.service';
+import _ from 'lodash';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 enum CupTab {
@@ -12,10 +13,19 @@ enum CupTab {
     TotoCup = 2,
 }
 
+/**
+ * view settings variables
+ * @returns
+ */
 const useViewState = () => {
+    const { navigate } = useAppNavigator();
     const [tab, setTab] = useState(CupTab.NationCup);
     const [cups, setCups] = useState<CupModel[]>([]);
     const [shouldClearCache, setClearChace] = useState(true); // First time when we change tab 1 to tab 2, should clear cache
+    const [onSelect, setOnSelect] = useState(0);
+
+    // const [searchText, setSearchText] = useState('');
+    // const searchTextRef = useRef<any>(null);
 
     return {
         tab,
@@ -24,31 +34,77 @@ const useViewState = () => {
         setCups,
         shouldClearCache,
         setClearChace,
+        navigate,
+        onSelect,
+        setOnSelect,
+        // searchText,
+        // setSearchText,
+        // searchTextRef,
     };
 };
 
-const useViewCallback = ({ setTab }: any) => {
-    const { navigate } = useAppNavigator();
+/**
+ * State use event handler
+ * @param state
+ * @param callback
+ * @returns
+ */
+const useEventHandler = (state: any, callback: any) => {
+    const { setTab, navigate, setCups, tab } = state;
+    const { searchCupsData } = callback;
     const changeTab = (index: number) => {
         const type = index === 0 ? CupTab.NationCup : CupTab.TotoCup;
-        setTab(type);
+        setTab(() => type);
     };
     const goToStateCupPage = (cup: CupModel) => {
         navigate(ScreenName.StateCupPage, { cup });
     };
+
+    const onChangeText = (text: string, tab: number) => {
+        searchCupsData(text, tab);
+    };
+
+    const onSearchCup = _.debounce(onChangeText, 500);
+
     return {
         changeTab,
         goToStateCupPage,
+        onSearchCup,
     };
 };
 
-export const useViewModel = ({ navigation, route }: IGobletScreenProps) => {
-    const { t } = useTranslation();
-    const { tab, setTab, cups, setCups, shouldClearCache, setClearChace } = useViewState();
-    const { changeTab, goToStateCupPage } = useViewCallback({ setTab, setCups });
-    const [onSelect, setOnSelect] = useState(0);
-    const { data: cupsData } = useCups(tab);
+/**
+ * Handle api calling here
+ * @param state
+ * @returns
+ */
+const useViewCallback = (state: any, route: any) => {
+    const { setCups } = state;
 
+    const searchCupsData = useCallback(async (text: string, tab: number) => {
+        console.log('TAb', tab);
+        const [error, res] = await cupsService.searchCups(text, tab);
+        if (error) {
+            return;
+        }
+
+        setCups(res.data.documents);
+    }, []);
+
+    return { searchCupsData };
+};
+
+/**
+ * Handle effect to listening variables change here.
+ * @param state
+ * @param eventHandler
+ * @param callback
+ */
+
+const useEffectHandler = (state: any, callback: any, eventHandler: any) => {
+    const { tab, setCups, shouldClearCache, setClearChace } = state;
+
+    const { data: cupsData } = useCups(tab);
     useEffect(() => {
         if (!cupsData) {
             return;
@@ -65,6 +121,15 @@ export const useViewModel = ({ navigation, route }: IGobletScreenProps) => {
             setClearChace(false);
         }
     }, [tab, shouldClearCache]);
+};
 
-    return { t, setOnSelect, goToStateCupPage, onSelect, changeTab, tab, cups };
+export const useViewModel = ({ navigation, route }: IGobletScreenProps) => {
+    const { t } = useTranslation();
+
+    const state = useViewState();
+    const callback = useViewCallback(state, route);
+    const eventHandler = useEventHandler(state, callback);
+    useEffectHandler(state, callback, eventHandler);
+
+    return { t, ...eventHandler, ...state };
 };
