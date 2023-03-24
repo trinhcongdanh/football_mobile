338832b44d8945a2dcbe9832e26719ecb4439fca
Quy Nguyen <phuquy.uit@gmail.com>
2023-03-24 09:30:10 +0700

    fix open home page magazine

--- src/app/screens/football-home/layouts/Item9/item9.viewModel.ts
+++ src/app/screens/football-home/layouts/Item9/item9.viewModel.ts
@@ -12,14 +12,9 @@ export const useViewModel = ({ homePage }: IItem9Props) => {
     const dots = Array(homePage.magazine.length).fill('');
 
     const onClickImage = useCallback(async (url: string) => {
-        // Checking if the link is supported for links with custom URL scheme.
-        const supported = await Linking.canOpenURL(url);
-
-        if (supported) {
-            // Opening the link with some app, if the URL scheme is "http" the web link should be opened
-            // by some browser in the mobile
+        try {
             await Linking.openURL(url);
-        } else {
+        } catch (error) {
             Alert.alert(`Don't know how to open this URL: ${url}`);
         }
     }, []);
