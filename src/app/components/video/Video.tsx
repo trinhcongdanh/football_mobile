375ef930d590920baa8655b832de316d179c59c9
trinhcongdanh <congdanhtrinh.dev@gmail.com>
2023-03-28 11:45:30 +0700

    fix: show youtube video

--- src/app/components/video/Video.tsx
+++ src/app/components/video/Video.tsx
@@ -3,147 +3,36 @@ import { AppImages } from '@football/app/assets/images';
 import { styles } from '@football/app/components/video/Video.style';
 import { appColors } from '@football/app/utils/constants/appColors';
 import { appStyles } from '@football/app/utils/constants/appStyles';
-import { useTranslationText } from '@football/app/utils/hooks/useLanguage';
 import { getSize } from '@football/app/utils/responsive/scale';
 import { BlurView } from '@react-native-community/blur';
 import Slider from '@react-native-community/slider';
-import React, { createRef, useEffect, useState } from 'react';
-import {
-    BackHandler,
-    I18nManager,
-    Platform,
-    ScrollView,
-    Text,
-    TouchableOpacity,
-    View,
-} from 'react-native';
+import React from 'react';
+import { I18nManager, ScrollView, Text, TouchableOpacity, View } from 'react-native';
 import FastImage from 'react-native-fast-image';
-import Orientation from 'react-native-orientation-locker';
 import IconFontAwesome5 from 'react-native-vector-icons/FontAwesome5';
 import IconIonicons from 'react-native-vector-icons/Ionicons';
-import Video_Player from 'react-native-video';
-import { useDispatch, useSelector } from 'react-redux';
-import { resetVideo, setHiddenVideo } from 'src/store/video/Video.slice';
+import YoutubePlayer from 'react-native-youtube-iframe';
+import { useViewModel } from './Video.viewModel';
 
 export const Video = () => {
-    // Pause Video
-    const dispatch = useDispatch();
-    const [pause, setPause] = useState(true);
-    const handlePause = () => {
-        setPause(!pause);
-    };
-
-    // console.log(pause);
-
-    // Next or back minute Video
-    const videoRef = createRef<any>();
-    const [currentTime, setCurrentTime] = useState(0);
-
-    const skipForward = () => {
-        videoRef.current.seek(currentTime + 15);
-        setCurrentTime(currentTime + 15);
-    };
-
-    const skipBackward = () => {
-        videoRef.current.seek(currentTime - 15);
-        setCurrentTime(currentTime - 15);
-    };
-
-    const [duration, setDuration] = useState(0);
-
-    const onSeek = (data: number) => {
-        videoRef.current.seek(data);
-        setCurrentTime(data);
-    };
-
-    const onLoadEnd = (data: any) => {
-        setDuration(data.duration);
-        setCurrentTime(data.currentTime);
-    };
-
-    const onProgress = (data: any) => {
-        setCurrentTime(data.currentTime);
-    };
-    const onEnd = () => {
-        setPause(true);
-        videoRef.current.seek(0);
-    };
-
-    const getMinutesFromSeconds = (time: any) => {
-        const minutes = time >= 60 ? Math.floor(time / 60) : 0;
-        const seconds = Math.floor(time - minutes * 60);
-
-        return `${minutes >= 10 ? minutes : `0${minutes}`}:${
-            seconds >= 10 ? seconds : `0${seconds}`
-        }`;
-    };
-
-    const position = getMinutesFromSeconds(currentTime);
-    const fullDuration = getMinutesFromSeconds(duration);
-
-    const showVideo = useSelector((state: any) => state.video.showVideo);
-    const sourceVideo = useSelector((state: any) => state.video.sourceVideo);
-    const [showControls, setShowControls] = useState(false);
-    const [fullscreen, setFullscreen] = useState(false);
-    const hiddenVideo = () => {
-        videoRef?.current?.seek(0);
-        setPause(true);
-        dispatch(setHiddenVideo(false));
-        dispatch(resetVideo(null));
-    };
-
-    const showFullscreen = () => {
-        if (Platform.OS === 'ios') {
-            setShowControls(false);
-            if (videoRef?.current?.presentFullscreenPlayer) {
-                videoRef?.current?.presentFullscreenPlayer();
-            }
-        } else {
-            setFullscreen(true);
-            setShowControls(true);
-        }
-        Orientation.lockToLandscapeLeft();
-    };
-
-    const handleOrientation = (orientation: string) => {
-        // if(orientation === 'LANDSCAPE-LEFT' || orientation === 'LANDSCAPE-RIGHT'){
-        //     setFullscreen(true);
-        //     setShowControls(true);
-        // }
-        // else {
-        // }
-    };
-
-    useEffect(() => {
-        if (showControls) {
-            Orientation.lockToLandscapeLeft();
-            videoRef.current.presentFullscreenPlayer();
-        } else if (Platform.OS === 'android') {
-            Orientation.lockToPortrait();
-        }
-    }, [fullscreen, showControls]);
-
-    useEffect(() => {
-        Orientation.addOrientationListener(handleOrientation);
-
-        return () => {
-            Orientation.removeOrientationListener(handleOrientation);
-        };
-    }, []);
-
-    const backAction = () => {
-        hiddenVideo();
-        return true;
-    };
-    useEffect(() => {
-        if (showVideo) {
-            BackHandler.addEventListener('hardwareBackPress', backAction);
-            return () => BackHandler.removeEventListener('hardwareBackPress', backAction);
-        }
-        console.log(showVideo);
-    }, [showVideo]);
-
-    const { getTranslationText } = useTranslationText();
+    const {
+        showVideo,
+        sourceVideo,
+        videoRef,
+        currentTime,
+        duration,
+        pause,
+        getTranslationText,
+        handlePause,
+        skipForward,
+        skipBackward,
+        onStateChange,
+        onSliderValueChange,
+        showFullscreen,
+        hiddenVideo,
+        formatTime,
+        onReady,
+    } = useViewModel({});
     return (
         <View
             style={{
@@ -175,38 +64,30 @@ export const Video = () => {
                         }}
                     >
                         {showVideo && (
-                            <Video_Player
-                                ref={videoRef}
-                                source={{ uri: sourceVideo ? sourceVideo.video_url : '' }}
-                                style={styles.background_video}
-                                paused={pause}
-                                resizeMode="cover"
-                                ignoreSilentSwitch="ignore"
-                                onLoad={onLoadEnd}
-                                onProgress={onProgress}
-                                onEnd={onEnd}
-                                controls={showControls}
-                                fullscreen={fullscreen}
-                                fullscreenOrientation="landscape"
-                                onFullscreenPlayerDidDismiss={() => {
-                                    // Orientation.unlockAllOrientations();
-                                    Orientation.lockToPortrait();
-                                    setShowControls(false);
-                                    setFullscreen(false);
-                                }}
-                            />
+                            <View pointerEvents="none">
+                                <YoutubePlayer
+                                    ref={videoRef}
+                                    height={getSize.m(210)}
+                                    play={pause}
+                                    videoId={sourceVideo.video_url.split('=')[1]}
+                                    initialPlayerParams={{ controls: false }}
+                                    onChangeState={onStateChange}
+                                    onReady={() => onReady}
+                                    onFullScreenChange={showFullscreen}
+                                />
+                            </View>
                         )}
                         <View style={styles.controls}>
                             <View
                                 style={[
-                                    appStyles.flex_row_space_center,
+                                    styles.progress_bar,
                                     {
                                         marginHorizontal: getSize.m(27),
                                     },
                                 ]}
                             >
                                 <View style={styles.container_minutes}>
-                                    <Text style={styles.minutes}>{fullDuration}</Text>
+                                    <Text style={styles.minutes}>{formatTime(duration)}</Text>
                                 </View>
                                 <View>
                                     <Slider
@@ -219,7 +100,7 @@ export const Video = () => {
                                         minimumValue={0}
                                         maximumValue={duration > 0 ? duration : 0}
                                         step={1}
-                                        onValueChange={time => onSeek(time)}
+                                        onValueChange={onSliderValueChange}
                                         onSlidingStart={handlePause}
                                         onSlidingComplete={handlePause}
                                         minimumTrackTintColor={appColors.blue_light}
@@ -229,7 +110,7 @@ export const Video = () => {
                                     />
                                 </View>
                                 <View style={styles.container_minutes}>
-                                    <Text style={styles.minutes}>{position}</Text>
+                                    <Text style={styles.minutes}>{formatTime(currentTime)}</Text>
                                 </View>
                             </View>
                             <View
@@ -250,7 +131,7 @@ export const Video = () => {
                                         />
                                     </TouchableOpacity>
                                 </View>
-                                <View style={appStyles.flex_row_align_center}>
+                                <View style={styles.progress_bar}>
                                     <TouchableOpacity onPress={skipForward}>
                                         <FastImage
                                             source={AppImages.img_backward_right}
@@ -265,7 +146,7 @@ export const Video = () => {
                                         onPress={handlePause}
                                         style={styles.container_pause}
                                     >
-                                        {pause ? (
+                                        {!pause ? (
                                             <FastImage
                                                 source={AppImages.img_vector}
                                                 resizeMode={FastImage.resizeMode.contain}
