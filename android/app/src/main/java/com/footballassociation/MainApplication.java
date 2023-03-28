55f38a7031dc48f5c69d7d8977bb7c5e3637d306
trinhcongdanh <congdanhtrinh.dev@gmail.com>
2023-03-28 16:33:45 +0700

    fix: fix run app

--- android/app/src/main/java/com/footballassociation/MainApplication.java
+++ android/app/src/main/java/com/footballassociation/MainApplication.java
@@ -14,7 +14,6 @@ import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 import com.facebook.react.modules.i18nmanager.I18nUtil;
 import com.oblador.vectoricons.VectorIconsPackage;
-import com.brentvatne.react.ReactVideoPackage;
 import com.airbnb.android.react.maps.MapsPackage;
 import org.wonday.orientation.OrientationActivityLifecycle;
 
