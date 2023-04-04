aaea3f5895346167f73f044738aceffdfd65b948
Quy Nguyen <phuquy.uit@gmail.com>
2023-04-04 14:30:31 +0700

    fix carousel

--- src/app/components/carousel/Carousel.tsx
+++ src/app/components/carousel/Carousel.tsx
@@ -3,6 +3,7 @@ import { appColors } from '@football/app/utils/constants/appColors';
 import { getSize } from '@football/app/utils/responsive/scale';
 import React, { useState } from 'react';
 import { Dimensions, I18nManager, View } from 'react-native';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import Carousel from 'react-native-reanimated-carousel';
 import styles from './Carousel.style';
 
@@ -30,49 +31,51 @@ export const CustomCarousel = ({
 
     const [active, setActive] = useState(defaultIndex);
     return (
-        <View style={{ alignItems: 'center' }}>
-            <Carousel
-                autoPlay={autoPlay}
-                defaultIndex={defaultIndex}
-                {...baseOptions}
-                loop={false}
-                data={data}
-                scrollAnimationDuration={1000}
-                mode="parallax"
-                panGestureHandlerProps={{
-                    activeOffsetX: [-10, 10],
-                }}
-                modeConfig={{
-                    parallaxScrollingScale: 1,
-                    parallaxScrollingOffset: widthPerItem ? widthPerItem * 0.92 : 50,
-                    parallaxAdjacentItemScale: 0.9,
-                }}
-                pagingEnabled
-                onSnapToItem={(index: number) => setActive(index)}
-                renderItem={renderItem}
-            />
+        <GestureHandlerRootView>
+            <View style={{ alignItems: 'center' }}>
+                <Carousel
+                    autoPlay={autoPlay}
+                    defaultIndex={defaultIndex}
+                    {...baseOptions}
+                    loop={false}
+                    data={data}
+                    scrollAnimationDuration={1000}
+                    mode="parallax"
+                    panGestureHandlerProps={{
+                        activeOffsetX: [-10, 10],
+                    }}
+                    modeConfig={{
+                        parallaxScrollingScale: 1,
+                        parallaxScrollingOffset: widthPerItem ? widthPerItem * 0.92 : 50,
+                        parallaxAdjacentItemScale: 0.9,
+                    }}
+                    pagingEnabled
+                    onSnapToItem={(index: number) => setActive(index)}
+                    renderItem={renderItem}
+                />
 
-            <View style={styles.dotContainer}>
-                {data.map((_, index) => {
-                    return (
-                        // eslint-disable-next-line react/no-array-index-key
-                        <View key={index}>
-                            <View
-                                style={[
-                                    styles.dot,
-                                    {
-                                        width: index === active ? getSize.m(18) : getSize.m(5),
-                                        backgroundColor:
-                                            index === active
-                                                ? activePageColor
-                                                : appColors.soft_grey,
-                                    },
-                                ]}
-                            />
-                        </View>
-                    );
-                })}
+                <View style={styles.dotContainer}>
+                    {data.map((_, index) => {
+                        return (
+                            // eslint-disable-next-line react/no-array-index-key
+                            <View key={index}>
+                                <View
+                                    style={[
+                                        styles.dot,
+                                        {
+                                            width: index === active ? getSize.m(18) : getSize.m(5),
+                                            backgroundColor:
+                                                index === active
+                                                    ? activePageColor
+                                                    : appColors.soft_grey,
+                                        },
+                                    ]}
+                                />
+                            </View>
+                        );
+                    })}
+                </View>
             </View>
-        </View>
+        </GestureHandlerRootView>
     );
 };
