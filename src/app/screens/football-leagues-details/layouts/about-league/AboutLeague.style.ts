c19d8c43fd553c7c7895be1645d8498ccb40476a
trinhcongdanh <congdanhtrinh.dev@gmail.com>
2023-04-17 16:58:07 +0700

    fix: fix dot

--- src/app/screens/football-leagues-details/layouts/about-league/AboutLeague.style.ts
+++ src/app/screens/football-leagues-details/layouts/about-league/AboutLeague.style.ts
@@ -1,7 +1,7 @@
 import { AppFonts } from '@football/app/assets/fonts';
 import { appColors } from '@football/app/utils/constants/appColors';
 import { getSize } from '@football/app/utils/responsive/scale';
-import { I18nManager, StyleSheet } from 'react-native';
+import { I18nManager, Platform, StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
     ic_football: {
@@ -55,7 +55,8 @@ const styles = StyleSheet.create({
         marginHorizontal: getSize.m(5),
     },
     dotContainer: {
-        flexDirection: I18nManager.isRTL ? 'row-reverse' : 'row',
+        flexDirection:
+            Platform.OS === 'android' ? (I18nManager.isRTL ? 'row-reverse' : 'row') : 'row',
         justifyContent: 'center',
         marginTop: getSize.m(16),
     },
